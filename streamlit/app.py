e18c96ee710ed63063a4cf0546aae9f34857acbd
Stetson Done <scdone@gmail.com>
2022-03-16 19:18:41 -0700

    troubleshooting deployment errors

--- streamlit/app.py
+++ streamlit/app.py
@@ -32,6 +32,8 @@ df_2018 = pd.read_csv('datasets\per_capita_2018.csv')
 df_2019 = pd.read_csv('datasets\per_capita_2019.csv')
 df_2020 = pd.read_csv('datasets\per_capita_2020.csv')
 
+
+
 # prep data for map visualizations
 
 state_dict = {'Alabama':'AL','Alaska':'AK','Arizona':'AZ','Arkansas':'AR','California':'CA','Colorado':'CO','Connecticut':'CT','Delaware':'DE','District of Columbia':'DC','Florida':'FL','Georgia':'GA','Idaho':'ID', 'Illinois':'IL','Indiana':'IN', 'Iowa':'IA','Kansas':'KS', 'Kentucky':'KY', 'Louisiana':'LA', 'Maine':'ME', 'Maryland':'MD','Massachusetts':'MA', 'Michigan':'MI', 'Minnesota':'MN', 'Mississippi':'MS', 'Missouri':'MO', 'Montana':'MT', 'Nebraska':'NE', 'Nevada':'NV', 'New Hampshire':'NH', 'New Jersey':'NJ', 'New Mexico':'NM', 'New York':'NY', 'North Carolina':'NC', 'North Dakota':'ND', 'Ohio':'OH','Oklahoma':'OK', 'Oregon':'OR','Pennsylvania':'PA', 'Rhode Island':'RI', 'South Carolina':'SC', 'South Dakota':'SD', 'Tennessee':'TN', 'Texas':'TX', 'Utah':'UT', 'Vermont':'VT', 'Virginia':'VA', 'Washington':'WA', 'West Virginia':'WV', 'Wisconsin':'WI', 'Wyoming':'WY'}
